429f65b92c4e605da306752805b806da792b4610
pulkit-servian <82551939+pulkit-servian@users.noreply.github.com>
2021-06-03 13:17:06 +1000

    Update backend.tf

--- AKS/backend.tf
+++ AKS/backend.tf
@@ -1,1 +1,9 @@
-
+#This defines the backend of terraform for storing its state files
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "aks-demo"
+    storage_account_name = "aksdemoterraform"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
