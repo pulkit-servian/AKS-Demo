39454b338a30c155df1da9effdb30d319440db27
pulkit-servian <pulkit.singh@servian.com>
2021-06-03 13:35:05 +1000

    adding comments

--- AKS/aks.tf
+++ AKS/aks.tf
@@ -1,10 +1,11 @@
+#Custom Vnet for AKS
 resource "azurerm_virtual_network" "aks_vnet" {
   name                = "aks-vnet"
   address_space       = ["10.1.0.0/16"]
   location            = var.location
   resource_group_name = var.resource_group_name
 }
-
+#Custom subnet within the AKS vnet
 resource "azurerm_subnet" "subnet" {
   name                 = "aks-subnet"
   resource_group_name  = var.resource_group_name
