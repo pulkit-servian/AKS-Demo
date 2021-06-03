f7d739360ec1214c36cb80cfc19122150bb02ae2
pulkit-servian <82551939+pulkit-servian@users.noreply.github.com>
2021-06-03 13:09:15 +1000

    Update providers.tf

--- AKS/providers.tf
+++ AKS/providers.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+provider "hcs" {
+}
+
+data "azurerm_client_config" "current" {}
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+}
